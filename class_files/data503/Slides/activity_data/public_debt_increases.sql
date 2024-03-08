645044f16048c19abd4a1fe408f58d0cada3e3ea
Jed Rembold <jedediah.r@gmail.com>
2024-03-07 16:58:09 -0800

    Add newest version of debt data

---
+++ class_files/data503/Slides/activity_data/public_debt_increases.sql
@@ -0,0 +1,148 @@
+CREATE TABLE public.debt_increases (
+  year INT,
+  president TEXT,
+  party TEXT,
+  debt_inc_pct REAL
+);
+;
+
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(0.2, 'Democrat', 'Harry S. Truman', 1949);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.8, 'Democrat', 'Harry S. Truman', 1950);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(-0.8, 'Democrat', 'Harry S. Truman', 1951);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.5, 'Democrat', 'Harry S. Truman', 1952);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2.7, 'Republican', 'Dwight D. Eisenhower', 1953);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2, 'Republican', 'Dwight D. Eisenhower', 1954);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.1, 'Republican', 'Dwight D. Eisenhower', 1955);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(-0.6, 'republican', 'Dwight D. Eisenhower', 1956);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(-0.8, 'Republican', 'Dwight D. Eisenhower', 1957);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2.1, 'Republican', 'Dwight D. Eisenhower', 1958);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(3, 'Republican', 'Dwight D. Eisenhower', 1959);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(0.6, 'Republican', 'Dwight D. Eisenhower', 1960);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(0.9, 'Democratic', 'John F. Kennedy', 1961);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(3.2, 'Democratic', 'John F. Kennedy', 1962);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2.6, 'Democratic', 'John F. Kennedy', 1963);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.9, 'Democratic', 'Lyndon B. Johnson', 1964);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.8, 'Democratic', 'Lyndon B. Johnson', 1965);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(0.8, 'Democratic', 'Lyndon B. Johnson', 1966);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2, 'Democratic', 'Lyndon B. Johnson', 1967);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(6.5, 'Democratic', 'Lyndon B. Johnson', 1968);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.8, 'Republican', 'Richard Nixon', 196);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(4.9, 'Republican', 'Richard Nixon', 1970);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.3, 'Republican', 'Richard Nixon', 1971);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.3, 'Republican', 'Richard Nixon', 1972);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.2, 'Republican', 'Richard Nixon', 1973);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(3.7, 'republican', 'Gerald Ford', 1974);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(12.2, 'republican', 'Gerald Ford', 1975);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(16.4, 'republican', 'Gerald Ford', 1976);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(16.4, 'republican', 'Gerald Ford', 1976);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(16.4, 'republican', 'Gerald Ford', 1976);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(12.6, 'Democrat', 'Jimmy Carter', 1877);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(10.4, 'Democrat', 'Jimmy Carter', 1978);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.1, 'Democrat', 'Jimmy Carter', 1979);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(9.8, 'Democrat', 'Jimmy Carter', 1980);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(9.9, NULL, 'Ronald Reagan', 1981);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(14.4, NULL, 'Ronald Reagan', 1982);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(20.6, NULL, 'Ronald Reagan', 1983);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(14.2, NULL, 'Ronald Reagan', 1984);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(16, NULL, 'Ronald Reagan', 1985);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(16.6, NULL, 'Ronald Reagan', 1986);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(10.6, NULL, 'Ronald Reagan', 1987);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(10.7, NULL, 'Ronald Reagan', 1988);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(9.8, 'Republican', 'George HW Bush', 1989);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(13.2, 'Republican', 'George H. W. Bush', 1990);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(13.4, 'Republican', 'George H W Bush', 1991);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(10.9, 'Republican', 'George H. W. Bush', 1992);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(8.5, 'Democratic', 'Bill Clinton', 1993);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(6.4, 'Democratic', 'Bill Clinton', 1994);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(6, 'Democratic', 'Bill Clinton', 1995);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(5, 'Democratic', 'Bill Clinton', 1996);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(3.6, 'Democratic', 'Bill Clinton', 1997);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2.1, 'Democratic', 'Bill Clinton', 1998);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2.4, 'Democratic', 'Bill Clinton', 1999);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(0.3, 'Democratic', 'Bill Clinton', 2000);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(2.3, 'Republican', 'George W. Bush', 2001);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.2, 'Republican', 'George W. Bush', 2002);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(8.9, 'Republican', 'George W. Bush', 2003);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(8.8, 'Republican', 'George W. Bush', 2004);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.5, 'Republican', 'George W. Bush', 2005);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(7.2, 'Republican', 'George W. Bush', 2006);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(5.9, 'republican', 'George W. Bush', 2007);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(11.3, 'republican', 'George W Bush', 2008);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(18.8, 'Democratic', 'Barack Obama', 2009);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(13.9, 'Democratic', 'Barack Obama', 2010);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(9.1, 'Democratic', 'Barack Obama', 2011);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(8.6, 'Democratic', 'Barack Obama', 2012);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(4.2, 'Democratic', 'Barack Obama', 2013);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(6.5, 'Democratic', 'Barack Obama', 2014);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(1.8, 'Democratic', 'Barack Obama', 2015);
+INSERT INTO "public"."debt_increases" ("debt_inc_pct", "party", "president", "year") VALUES 
+(3.7, 'Democratic', 'Barack Obamabama', 2016);
