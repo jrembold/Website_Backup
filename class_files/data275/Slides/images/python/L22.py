9f7658e72de2036fd54b503cf1249337cb47ff71
Jed Rembold <jedediah.r@gmail.com>
2025-01-16 12:43:01 -0800

    Add lec2 slides

--- class_files/data275/Slides/images/python/L22.py
+++ class_files/data275/Slides/images/python/L22.py
@@ -9,18 +9,20 @@ matplotlib.rc('ytick', labelsize=16)
 
 
 def gen_data():
-    xs = np.sort(np.random.uniform(-5,5,size=100))
-    a_true = -3
+    xs = np.sort(np.random.uniform(-5,5,size=50))
+    a_true = -1.5
     b_true = 5
-    dev = np.random.normal(0,10, size=xs.size)
     y_true = a_true * xs ** 2 + b_true * xs
+    dev = np.random.normal(0, 20, size=xs.size)
     y = y_true + dev
-    errs = np.abs(y-y_true + np.random.normal(0,1,size=xs.size))
+    errs = np.abs(1.5*dev + np.random.normal(0,1,size=xs.size))
 
     data = pd.DataFrame({'x': xs, 'y': y, 'y_err': errs})
     data.to_csv('mcmc_fit_example.csv', index=False)
 
     plt.errorbar(xs, y, yerr=errs, fmt='.')
+    plt.xlabel('x', fontsize=22)
+    plt.ylabel('y', fontsize=22)
     plt.savefig('mcmc_fit_example.png', facecolor='#ffffff00', bbox_inches='tight')
     plt.show()
 
